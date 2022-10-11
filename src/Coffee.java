883407cc658b83ac0a35a944f2209e0c685c7109
weemutasim <asmin_maha@outlook.com>
2022-10-11 15:10:52 +0700

    first commit

---
+++ src/Coffee.java
@@ -0,0 +1,28 @@
+public class Coffee {
+    private String name;
+    private double price;
+
+    Coffee(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+   
+
+
+}
