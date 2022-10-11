45ed629452d0357b7aaa2a6b4bbe359da96dfbd9
weemutasim <asmin_maha@outlook.com>
2022-10-11 15:06:37 +0700

    add case 1 Main

--- src/CallingMain.java
+++ src/CallingMain.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 public class CallingMain {
     public static void main(String[] args) {
-        // clear the output Screen
+        //clear the output Screen
         //System.out.print("\033[H\033[2J");
 
         Scanner rk = new Scanner(System.in);
         Admin am = new Admin(rk);
         Product pd = new Product(rk);
+        Pay py = new Pay();
 
         String password;
         password = "123";
 
-        // Callining Mainmenu Class function
+        // Calling Main Class function
         while (true) {
             am.printWelcome();
             if (am.getSelect() == 3) {
@@ -21,17 +22,27 @@ public class CallingMain {
             }
             switch (am.getSelect()) {
                 case 1:
-                    pd.printInfo(0);
+                    pd.printInfo("DataStock.txt", 0);
+                    System.out.print("\nPlace Your Order >>> ");
+                    String messageID = rk.next();
+                    pd.searchData("DataStock.txt", messageID);
+
+                    System.out.println("Enter amount of product : ");
+                    py.setAmount(rk.nextInt());
+                    System.out.println("Enter amount of paid : ");
+                    py.setPaid(rk.nextInt());
+
                     break;
                 case 2:
                     am.loginMain();
                     if (am.getPass().equals(password)) {
-                        System.out.println("Access Granted! Welcome!");
+                        //System.out.print("\033[H\033[2J");
+                        System.out.println("       Access Granted! Welcome!");
                         do {
                             am.printMenu();
                             switch (am.getSlmenu()) {
                                 case 1:
-                                    
+                                    //pd.searchData(password, 0);
                                     break;
                                 case 2:
                                     pd.getInfo();
@@ -47,13 +58,14 @@ public class CallingMain {
                                     pd.editData("DataStock.txt", search_id, new_name, new_price);
                                     break;
                                 case 4:
-                                    pd.printInfo(1);    //admin 0, costumer 1
+                                    pd.printInfo("DataStock.txt", 0);
                                     System.out.print("\nDelete Item >> ");
                                     int delete = rk.nextInt();
                                     pd.removeData("DataStock.txt", delete);
                                     break;
                                 case 5:
-                                    pd.printInfo(1);
+                                    pd.printInfo("DataStock.txt", 1);
+                                    rk.nextLine();
                                     break;
                                 case 6:
                                 
